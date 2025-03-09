e08c899bea7feef5efcfd1ea745145bfb38f156e
Pedro <30800044+pricci1@users.noreply.github.com>
2025-03-09 13:11:00 -0300

    feat: use Button component

--- app/javascript/pages/Poll/New.tsx
+++ app/javascript/pages/Poll/New.tsx
@@ -1,6 +1,7 @@
 import { Head, Link } from '@inertiajs/react'
 import Form from './Form'
 import { PollType } from './types'
+import { Button } from '@/components/ui/button'
 
 interface NewProps {
   poll: PollType
@@ -24,9 +25,9 @@ export default function New({ poll }: NewProps) {
 
       <br />
 
-      <div>
+      <Button asChild variant="secondary">
         <Link href="/polls">Back to polls</Link>
-      </div>
+      </Button>
     </>
   )
 }
